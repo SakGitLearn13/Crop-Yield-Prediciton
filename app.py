df24ff4f9f98ae4dcd0bf3153e02b9a5266e7a24
SakLearn <SakLearn@1312@>
2024-05-30 14:13:38 +0530

    Final Commit

--- app.py
+++ app.py
@@ -11,31 +11,6 @@ preprocessor = pickle.load(open('model/preprocessor.pkl', 'rb'))
 # creating flask app
 app = Flask(__name__)
 
-# Function to get current year
-def get_current_year():
-    return datetime.datetime.now().year
-
-# Function to get fertilizer and pesticide data
-def get_fertilizer_pesticide_data(State, District, Crop, Season):
-
-    # Load the JSON file into a DataFrame
-    grouped_df = pd.read_json('data\grouped_df.json')
-
-    # Filter data for the specified State, District, Crop, and Season
-    filtered_data = grouped_df[(grouped_df['State'] == State) & 
-                               (grouped_df['District'] == District) & 
-                               (grouped_df['Crop'] == Crop) &
-                               (grouped_df['Season'] == Season)]
-
-    if not filtered_data.empty:
-        # Extract the Fertilizer and Pesticide values
-        avg_fertilizer = filtered_data.iloc[0]['Avg_Fertilizer']
-        avg_pesticide = filtered_data.iloc[0]['Avg_Pesticide']
-        return avg_fertilizer, avg_pesticide
-    else:
-        return None, None
-    
-    return fertilizer_data, pesticide_data
 
 @app.route('/')
 def index():
@@ -56,7 +31,7 @@ def predict():
         Avg_rainfall = request.form['Avg_rainfall']
         Temperature = request.form['Temperature']
         Area = request.form['Area']
-        Production = request.form['Production']
+        
         # Get current year
         Year = get_current_year()
 
@@ -72,10 +47,9 @@ def predict():
             'Avg_rainfall': [Avg_rainfall],
             'Temperature': [Temperature],
             'Area': [Area],
-            'Production': [Production]
         }
         features = pd.DataFrame(feature_dict)
-
+        print(feature_dict)
         # Preprocessing of user input
         transformed_features = preprocessor.transform(features)
 
@@ -107,6 +81,32 @@ def get_crops():
     data = pd.read_csv('data/Crop_Data.csv')
     crops = data['Crop'].unique().tolist()
     return jsonify(crops=crops)
+
+# Function to get current year
+def get_current_year():
+    return datetime.datetime.now().year
+
+# Function to get fertilizer and pesticide data
+def get_fertilizer_pesticide_data(State, District, Crop, Season):
+
+    # Load the JSON file into a DataFrame
+    grouped_df = pd.read_json('data\grouped_df.json')
+
+    # Filter data for the specified State, District, Crop, and Season
+    filtered_data = grouped_df[(grouped_df['State'] == State) & 
+                               (grouped_df['District'] == District) & 
+                               (grouped_df['Crop'] == Crop) &
+                               (grouped_df['Season'] == Season)]
+
+    if not filtered_data.empty:
+        # Extract the Fertilizer and Pesticide values
+        avg_fertilizer = filtered_data.iloc[0]['Avg_Fertilizer']
+        avg_pesticide = filtered_data.iloc[0]['Avg_Pesticide']
+        return avg_fertilizer, avg_pesticide
+    else:
+        return None, None
+    
+    return fertilizer_data, pesticide_data
 
 # python main
 if __name__ == '__main__':
